13ee55455936fad6cc6d09d292d476386f94870e
neofytr <imagination12344321.com@gmail.com>
2025-06-23 18:19:10 +0530

    some observations

--- syscalls/inc/sys.h
+++ syscalls/inc/sys.h
@@ -6,6 +6,22 @@
                     // so, we will get the correct int sizes for that machine when using standard C integers
 
 typedef uint16_t fd_t;
+typedef uint16_t err_t;
+
+err_t errno;
+
+// our OS api functions will return zero on error and 1 on success
+// the global variable errno will be set to indicate the error in the most recent
+// failed call to the system
+
+// making this variable global results in making the entire OS single-threaded
+
+#define reterr(x)    \
+    do               \
+    {                \
+        errno = (x); \
+        return 0;    \
+    } while (false)
 
 // this file contains the headers for the system calls in the operating system neoSys
 uint8_t load(fd_t file);               // read one byte from the file descriptor file_desc
