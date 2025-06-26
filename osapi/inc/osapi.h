232f5664a6a5f3d81c9ac02566c5ce89d6a111ba
neofytr <imagination12344321.com@gmail.com>
2025-06-27 01:38:43 +0530

    new osapi header

--- osapi/inc/osapi.h
+++ osapi/inc/osapi.h
@@ -5,17 +5,20 @@
 #include <sys.h>
 #include <stdint.h>
 
+// maximum number of file descriptors supported by neosys
 #define MAX_FD (1U << 8)
 
 #ifdef INSIDE_SYS
 
+// maps neosys file descriptors to posix file descriptors
+// file_desc_arr[file] holds the posix fd mapped to neosys fd 'file'
+// -1 indicates no mapping exists for that neosys fd
 internal int file_desc_arr[MAX_FD] = {
-    0,
-    1,
-    2,
-    -1,
-}; // file_desc_arr[file] is the posix fd mapped to the neoSys fd file
-   // it's -1 if  there is no mapping
+    0,  // neosys fd 0 -> posix stdin
+    1,  // neosys fd 1 -> posix stdout
+    2,  // neosys fd 2 -> posix stderr
+    -1, // remaining slots unmapped
+};
 
 #else
 
@@ -23,18 +26,32 @@ extern int *file_desc_arr;
 
 #endif
 
-// the neoSys fd 0, 1, and 2 are the same as the posix fd 0, 1, and 2 respectively
-// neoSys fd 0 -> stdin
-// neoSys fd 1 -> stdout
-// neoSys fd 2 -> stderr
+// standard file descriptor mappings (same as posix):
+// neosys fd 0 -> stdin  (standard input)
+// neosys fd 1 -> stdout (standard output)
+// neosys fd 2 -> stderr (standard error)
 
+// macro to get the posix fd corresponding to a neosys fd
 #define get_posix_fd(x) (file_desc_arr[(x)])
 
-internal uint16_t stringlen(void *str);                       // length of a traditional null-terminated C-string
-internal bool isopen(const fd_t file);                        // check if the file desc file points to an open file
-internal bool zero(void *mem, uint16_t bytes);                // zeroes bytes number of bytes, starting at mem
-internal bool copy(void *dest, void *source, uint16_t bytes); // copies bytes number of bytes from source to destination; correctly handles overlapping regions
-internal uint8_t *strnum(uint8_t *str, uint8_t num);          // returns a pointer to a static piece of memory (static string) that is the string str with the number num in decimal appended to it
-                                                              // the maximum length of the final string is 256; so, overflow can occur if the binary rep of the number makes the string longer than 256, in which case, the
-                                                              // requisite lower digits of num will be left out in the resultant string
+// returns the length of a null-terminated c string
+internal uint16_t stringlen(void *str);
+
+// checks if the given neosys file descriptor points to an open file
+internal bool isopen(const fd_t file);
+
+// zeroes 'bytes' number of bytes starting at memory address 'mem'
+internal bool zero(void *mem, uint16_t bytes);
+
+// copies 'bytes' from 'source' to 'dest' memory locations
+// handles overlapping memory regions correctly
+internal bool copy(void *dest, void *source, uint16_t bytes);
+
+// appends the decimal representation of 'num' to string 'str'
+// returns pointer to a static buffer containing the result
+// maximum result length is 256 characters
+// if the final string would exceed 256 chars, the lower digits
+// of 'num' are truncated to fit within the limit
+internal uint8_t *strnum(uint8_t *str, uint8_t num);
+
 #endif
