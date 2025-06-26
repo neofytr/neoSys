92450a7ecd349aa13ab8ba3b1e15d746afb941fb
neofytr <imagination12344321.com@gmail.com>
2025-06-26 15:42:43 +0530

    some info

--- neo.c
+++ neo.c
@@ -7,9 +7,12 @@
 #define SRC "src/"
 #define SHELL "shell/"
 #define SYS "syscalls/"
+#define EMU "disk_emulator/"
 #define COMMON "common/"
 #define INC "inc/"
 #define BIN "bin/"
+
+#define SO_FLAGS "-ldl -fPIC -shared" // create a shared library, with support for dynamic loading
 
 #define CFLAGS "-O2 -Wall "     \
                "-I " SYS INC    \
@@ -19,11 +22,13 @@
 
 int main(int argc, char **argv)
 {
+    bool ret;
+    int retval;
     neorebuild("neo.c", argv, &argc);
 
     if (argc > 1 && !strcmp(argv[1], "clean"))
     {
-        int retval = system("rm " BIN "shell.neo " SHELL SRC "shell.o " SYS SRC "sys.o");
+        retval = system("rm " BIN "shell.neo " SHELL SRC "shell.o " SYS SRC "sys.o " BIN "osapi.so");
         if (retval < 0)
         {
             fprintf(stderr, "CLEAN FAILED!\n");
@@ -34,7 +39,7 @@ int main(int argc, char **argv)
         return EXIT_SUCCESS;
     }
 
-    bool ret = neo_compile_to_object_file(GCC, SHELL SRC "shell.c", NULL, CFLAGS, false);
+    ret = neo_compile_to_object_file(GCC, SHELL SRC "shell.c", NULL, CFLAGS, false);
     if (!ret)
     {
         return EXIT_FAILURE;
@@ -51,6 +56,19 @@ int main(int argc, char **argv)
     {
         return EXIT_FAILURE;
     }
+
+    ret = neo_compile_to_object_file(GCC, EMU SRC "emu.c", NULL, CFLAGS, false);
+    if (!ret)
+    {
+        return EXIT_FAILURE;
+    }
+
+    // now we make it a shared library
+    neocmd_t cmd;
+
+    neocmd_append(&cmd, "gcc", "-o bin/osapi.so");
+    neocmd_append(&cmd, SO_FLAGS);
+    neocmd_append(&cmd, "sys.o emu.o");
 
     return EXIT_SUCCESS;
 }
