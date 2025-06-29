d12ec60a3c4ef32b2881c0e844379261fd39c02d
neofytr <imagination12344321.com@gmail.com>
2025-06-29 18:14:16 +0530

    some diskutil

--- utils/diskutil/src/diskutil.c
+++ utils/diskutil/src/diskutil.c
@@ -3,6 +3,9 @@
 #include <stdint.h>
 #include <string.h>
 
+#include <disk.h>
+#include <filesys.h>
+
 void usage(uint8_t *arg);
 void usage_format(uint8_t *arg);
 void cmd_format(uint8_t *, uint8_t *);
@@ -15,6 +18,73 @@ void usage(uint8_t *arg)
                     "1. format\n");
 
     exit(EXIT_FAILURE);
+}
+
+void cmd_format(uint8_t *arg1, uint8_t *arg2)
+{
+    uint8_t drive;
+    uint8_t *drive_str;
+    drive_t *drive_desc;
+    filesys_t *filesys;
+
+    bool bootable;
+    bool force;
+
+    if (!arg1)
+        usage_format("diskutil");
+    if (!arg2)
+    {
+        bootable = false;
+        drive_str = arg1;
+    }
+    else
+    {
+        if (!strcmp(arg1, "-s"))
+        {
+            bootable = true;
+            drive_str = arg2;
+        }
+        else
+            usage_format("diskutil");
+    }
+
+    switch (*drive_str)
+    {
+    case ('c' || 'C'):
+        drive = 1;
+        break;
+    case ('D' || 'd'):
+        drive = 2;
+        break;
+    default:
+        usage_format("diskutil");
+    } // based on the first character of drive_string
+
+    if (bootable)
+    {
+        fprintf(stderr, "Bootable drives currently not supported\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(stdout, "This will format and ERASE your drive %s\n", drive_str);
+    fprintf(stdout, "Continue? (y/n)");
+    fflush(stdout);
+
+    scanf("%d\n", &force);
+    force = (force == 'y' || force == 'Y') ? true : false;
+    if (!force)
+        return;
+
+    fprintf(stdout, "Formatting drive %s\n", drive_str);
+    drive_desc = d_attach(drive);
+    if (!drive_desc)
+        fprintf(stderr, "Bad drive %s\n", drive_str) && return EXIT_FAILURE;
+
+    filesys = fs_format(drive_desc, NULL, true);
+    if (!filesys)
+        fprintf(stderr, "Error formatting the drive %s\n", drive_str) && return EXIT_FAILURE;
+
+    return EXIT_SUCCESS;
 }
 void usage_format(uint8_t *arg)
 {
@@ -45,6 +115,8 @@ int main(int argc, uint8_t **argv)
 
     if (!strcmp(cmd, "format"))
         cmd_format(arg1, arg2);
+    else
+        usage(argv[0]);
 
     return EXIT_SUCCESS;
 }
